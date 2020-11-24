3b5292c923fe4c74f2e051da2f19d27e2990d59f
Daniel Nuttall <daniel.nuttall@answerdigital.com>
2020-11-24 14:49:39 +0000

    Fixed parent/child display on DataModel "Properties" tab

--- FrontEndv8/src/app/components/data-model-tabular-view/data-model-tabular-view.component.ts
+++ FrontEndv8/src/app/components/data-model-tabular-view/data-model-tabular-view.component.ts
@@ -1,13 +1,13 @@
+import { ConceptReferenceNode } from './../../models/objectmodel/ConceptReferenceNode';
 import { NgEventBus } from 'ng-event-bus';
-import {Component, Input, Output, EventEmitter} from '@angular/core';
-import {ConceptService} from '../../services/concept.service';
-import {DataModelDefinition, FlatProperty} from '../../models/old/DataModelDefinition';
-import {Router} from '@angular/router';
-import {LoggerService} from 'dds-angular8/logger';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { ConceptService } from '../../services/concept.service';
+import { DataModelDefinition, FlatProperty } from '../../models/old/DataModelDefinition';
+import { Router } from '@angular/router';
+import { LoggerService } from 'dds-angular8/logger';
 import { MatTableDataSource } from '@angular/material/table';
 import { animate, state, style, transition, trigger } from '@angular/animations';
-import { Observable } from 'rxjs';
-import { Concept} from '../../models/objectmodel/Concept';
+import { Concept } from '../../models/objectmodel/Concept';
 
 const debug = (message: string) => { console.log(message); };
 
@@ -17,16 +17,14 @@ const debug = (message: string) => { console.log(message); };
   styleUrls: ['./data-model-tabular-view.component.scss'],
   animations: [
     trigger('detailExpand', [
-      state('collapsed', style({height: '0px', minHeight: '0'})),
-      state('expanded', style({height: '*'})),
+      state('collapsed', style({ height: '0px', minHeight: '0' })),
+      state('expanded', style({ height: '*' })),
       transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
     ]),
   ],
 })
 
 class DataModelTablularViewComponent {
-  private static UNDEFINED_CONCEPT: Concept;
-
   private iri: string;
 
   dataModelDefinition: DataModelDefinition;
@@ -37,37 +35,48 @@ class DataModelTablularViewComponent {
   parentsChipListTemplateContext: ChipListTemplateContext;
   childrenChipListTemplateContext: ChipListTemplateContext;
 
-  @Input()
-  concept: Concept;
+  @Input() concept: Concept;
+  @Input() parents: Array<ConceptReferenceNode>;
+  @Input() children: Array<ConceptReferenceNode>;
 
   @Input()
   set conceptIri(iri: string) {
-      this.iri = iri;
-      this.refresh();
+    this.iri = iri;
+    this.refresh();
   }
 
   constructor(private service: ConceptService, private log: LoggerService, private router: Router, private eventBus: NgEventBus) {
-    this.propertiesTable = new DataTable<FlatProperty>(["name", "type", "cardinality", "description", "declaredOn"]);
+    this.propertiesTable = new DataTable<FlatProperty>(['name', 'type', 'cardinality', 'description', 'declaredOn']);
 
-    this.parentsChipListTemplateContext =  {
-      title: "Parents",
+    this.parentsChipListTemplateContext = {
+      title: 'Parents',
       chips: []
     };
 
-    this.childrenChipListTemplateContext =  {
-      title: "Children",
+    this.childrenChipListTemplateContext = {
+      title: 'Children',
       chips: []
     };
   }
 
   refresh() {
-    if(this.concept.SubClassOf[0].Intersection != null) {
+    if (this.concept.SubClassOf[0].Intersection != null) {
       this.concept.SubClassOf[0].Intersection.forEach(intersection => {
-        if(intersection.ObjectPropertyValue != null) {
+        if (intersection.ObjectPropertyValue != null) {
           this.propertiesTableData.push(intersection);
         }
       });
     }
+
+    this.parentsChipListTemplateContext = {
+      title: 'Parents',
+      chips: this.parents
+    };
+
+    this.childrenChipListTemplateContext = {
+      title: 'Children',
+      chips: this.children
+    };
   }
 
   onSelect(concept: Concept) {
@@ -92,7 +101,7 @@ class DataTable<D> {
     this.columns = columns;
   }
 
-  setRows(rows: D[]){
+  setRows(rows: D[]) {
     this.rows.data = rows;
   }
 
@@ -100,7 +109,7 @@ class DataTable<D> {
     this.rows.data = [];
   }
 
-  hasRows() : boolean {
+  hasRows(): boolean {
     return this.rows && this.rows.data.length > 0;
   }
 
@@ -114,12 +123,12 @@ class DataTable<D> {
 }
 
 interface ChipListTemplateContext {
-  title: string,
-  chips: Concept[]
+  title: string;
+  chips: ConceptReferenceNode[];
 }
 
 export {
   DataModelTablularViewComponent,
   DataTable,
   ChipListTemplateContext
-}
+};
