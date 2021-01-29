6c1d12ce249c0b496bd98954ecc3ef5bc3f6b2a3
Will Weatherill <will.weatherill@answerdigital.com>
2021-01-29 16:39:06 +0000

    search - removed search in top toolbar and fixed compilation error

--- FrontEndv8/src/app/app.module.ts
+++ FrontEndv8/src/app/app.module.ts
@@ -1,31 +1,31 @@
 /* Add Amplify imports */
-import {AmplifyUIAngularModule} from '@aws-amplify/ui-angular';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import { AmplifyUIAngularModule } from '@aws-amplify/ui-angular';
+import { NgJsonEditorModule } from 'ang-jsoneditor';
 import Amplify from 'aws-amplify';
+import { NgEventBus } from 'ng-event-bus';
+import { CookieService } from 'ngx-cookie-service';
+import { MonacoEditorModule } from 'ngx-monaco-editor';
 import awsconfig from '../aws-exports';
+import { AppRoot } from './app-root';
+import { ComponentsModule } from './components/components.module';
+import { LayoutComponent } from './layout/layout.component';
+import { LayoutModule } from './layout/layout.module';
+import { monacoConfig } from './monaco';
+import { AuthGuard } from './security/auth-guard';
+import { ConfirmComponent } from './security/confirm/confirm.component';
+import { LoginComponent } from './security/login/login.component';
+import { RegisterComponent } from './security/register/register.component';
+import { RouteGuard } from './security/route-guard';
+import { SecurityModule } from './security/security.module';
+import { SecurityService } from './security/security.service';
+import { LoggerService } from './services/logger.service';
+import { Perspectives } from './services/perspective.service';
 
-import {NgJsonEditorModule} from 'ang-jsoneditor';
-import {NgModule} from '@angular/core';
-import {RouterModule} from '@angular/router';
-import {HttpClientModule} from '@angular/common/http';
-import {AppRoot} from './app-root';
-import {CommonModule} from '@angular/common';
-import {SecurityService} from './security/security.service';
-import {NgEventBus} from 'ng-event-bus';
-import {LoggerService} from './services/logger.service';
-import {Perspectives} from './services/perspective.service';
-import {MonacoEditorModule} from 'ngx-monaco-editor';
-import {monacoConfig} from './monaco';
-import {SecurityModule} from './security/security.module';
-import {CookieService} from 'ngx-cookie-service';
-import {ComponentsModule} from './components/components.module';
-import {LoginComponent} from './security/login/login.component';
-import {RegisterComponent} from './security/register/register.component';
-import {ConfirmComponent} from './security/confirm/confirm.component';
-import {LayoutComponent} from './layout/layout.component';
-import {AuthGuard} from './security/auth-guard';
-import {RouteGuard} from './security/route-guard';
-import {LayoutModule} from './layout/layout.module';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 /* Configure Amplify resources */
 Amplify.configure(awsconfig);
@@ -49,7 +49,6 @@ let routes = [
       // Main paths/pages
       {path: 'mainPage', loadChildren: () => import('./main-page/main-page.module').then(m => m.MainPageModule), data: {role: 'information-manager:conceptLibrary', helpContext: '#'}},
       {path: 'healthRecordOverview', loadChildren: () => import('./pages/health-record-overview/health-record-overview.module').then(m => m.HealthRecordOverviewModule), data: {role: 'information-manager:conceptLibrary', helpContext: '#Data_models'}},
-      {path: 'search', loadChildren: () => import('./pages/search/search.module').then(m => m.SearchModule), data: {role: 'information-manager:conceptLibrary', helpContext: '#Search'}},
       {path: 'ontology/:id', loadChildren: () => import('./pages/ontology/ontology.module').then(m => m.OntologyModule), data: {role: 'information-manager:conceptLibrary', helpContext: '#Semantic_ontology'}},
       {path: 'healthRecord/:id', loadChildren: () => import('./pages/health-record/health-record.module').then(m => m.HealthRecordModule), data: {role: 'information-manager:conceptLibrary', helpContext: '#Data_models'}},
       {path: 'valueSets/:id', loadChildren: () => import('./pages/value-sets/value-set.module').then(m => m.ValueSetModule), data: {role: 'information-manager:conceptLibrary', helpContext: '#Value_sets'}},
