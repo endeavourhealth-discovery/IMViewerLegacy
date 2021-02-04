b5c1f21c7dff9d0cc0d60bf55f1721d283e73ef3
Richard Collier <gitkraken@tirinoarim.co.uk>
2021-02-04 09:44:37 +0000

    Added new concept types

--- FrontEndv8/src/app/models/objectmodel/ConceptType.ts
+++ FrontEndv8/src/app/models/objectmodel/ConceptType.ts
@@ -4,5 +4,10 @@ export enum ConceptType {
   DataProperty = 'DataProperty',
   DataType = 'DataType',
   Annotation = 'Annotation',
-  Individual = 'Individual'
+  Individual = 'Individual',
+  Record = 'Record',
+  ValueSet = 'ValueSet',
+  Folder = 'Folder',
+  Term  = 'Term',
+  Legacy = 'Legacy'
 }
