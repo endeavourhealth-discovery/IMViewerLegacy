455a30925daa9a3bca4c9c85336b7de6018e8b6b
matt-ergosoft <matt.hutcheson@ergosoft.co.uk>
2021-03-16 16:02:31 +0000

    added save and cancel buttons to ConceptDetails component

--- front-end-vue/src/components/conceptDetails/ConceptDetails.vue
+++ front-end-vue/src/components/conceptDetails/ConceptDetails.vue
@@ -1,10 +1,18 @@
 <template>
   <Card>
+    <template #header>
+      <Button icon="pi pi-check" label="Save" class="p-button" style="width: fit-content; margin: 14px 0 14px 14px;"/>
+      <Button icon="pi pi-times" label="Cancel" class="p-button-secondary" style="width: fit-content; margin: 14px;"/>
+    </template>
     <template #title>
       Concept details
     </template>
     <template #content v-if="concept && schema">
       <FormGenerator :schema="schema" :concept="concept"/>
+    </template>
+    <template #footer>
+      <Button icon="pi pi-check" label="Save" class="p-button" style="width: fit-content;"/>
+      <Button icon="pi pi-times" label="Cancel" class="p-button-secondary" style="width: fit-content; margin-left: 14px;"/>
     </template>
   </Card>
 </template>
