413e465b4f6aac749310066e43379cb601048435
matt-ergosoft <matt.hutcheson@ergosoft.co.uk>
2021-03-19 11:20:48 +0000

    added setId to user model

--- front-end-vue/src/models/User.ts
+++ front-end-vue/src/models/User.ts
@@ -22,6 +22,10 @@ export class User {
     this.id = "";
   }
 
+  setId(id: string){
+    this.id = id;
+  }
+
   verifyEmail(email: any){
     if (/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)){
       return email;
