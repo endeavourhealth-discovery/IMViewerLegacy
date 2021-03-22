b991a2bc8d5fa5543ee6ab39e06b685b47536fca
matt-ergosoft <matt.hutcheson@ergosoft.co.uk>
2021-03-22 15:19:50 +0000

    added home button to user view

--- front-end-vue/src/views/user/User.vue
+++ front-end-vue/src/views/user/User.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="p-d-flex p-flex-column p-ai-center p-jc-center user-container">
     <div class="p-d-flex p-flex-column p-jc-start card-button-container">
-      <Button class="back-button" label="back" icon="pi pi-arrow-circle-left" iconPos="left" v-on:click.prevent="$router.go(-1)" />
+      <div class="p-d-flex p-flex-row p-jc-start">
+        <Button class="back-button" label="back" icon="pi pi-arrow-circle-left" iconPos="left" v-on:click.prevent="$router.go(-1)" />
+        <Button class="home-button" icon="pi pi-home" v-on:click.prevent="$router.push({name: 'Home'})" />
+      </div>
       <router-view />
     </div>
   </div>
@@ -31,6 +34,10 @@ export default class User extends Vue {
 
 .back-button {
   width: fit-content;
+}
+
+.home-button {
+  margin-left: 0.25em;
 }
 
 </style>
