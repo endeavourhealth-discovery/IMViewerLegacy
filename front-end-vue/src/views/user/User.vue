ddeb9f3e1ce70ddf76ba59e54c26c460c6e15450
matt-ergosoft <matt.hutcheson@ergosoft.co.uk>
2021-03-22 13:33:00 +0000

    added back button for user pages

--- front-end-vue/src/views/user/User.vue
+++ front-end-vue/src/views/user/User.vue
@@ -1,6 +1,9 @@
 <template>
-  <div class="p-d-flex p-flex-row p-jc-center user-container">
+  <div class="p-d-flex p-flex-column p-ai-center p-jc-center user-container">
+    <div class="p-d-flex p-flex-column p-jc-start card-button-container">
+      <Button class="back-button" label="back" icon="pi pi-arrow-circle-left" iconPos="left" v-on:click.prevent="$router.go(-1)" />
       <router-view />
+    </div>
   </div>
 </template>
 
@@ -24,6 +27,10 @@ export default class User extends Vue {
 
 .user-container {
   height: 100vh;
+}
+
+.back-button {
+  width: fit-content;
 }
 
 </style>
