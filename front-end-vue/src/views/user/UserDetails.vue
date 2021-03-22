8f5994f07bdbc3a3d0ddb82d8e4f3474b24dd87c
matt-ergosoft <matt.hutcheson@ergosoft.co.uk>
2021-03-22 13:56:22 +0000

    added show user details card and route to edit page function

--- front-end-vue/src/views/user/UserDetails.vue
+++ front-end-vue/src/views/user/UserDetails.vue
@@ -1,5 +1,21 @@
 <template>
-  <h1>UserDetails</h1>
+  <div v-if="currentUser" class="p-d-flex p-flex-row p-ai-center">
+    <Card class="p-d-flex p-flex-column p-jc-sm-around p-ai-center user-details-card">
+      <template #header>
+        <i class="pi pi-fw pi-user" style="fontSize: 50px; margin: 1em;" />
+      </template>
+      <template #title>
+        My Account Details
+      </template>
+      <template #content>
+        <InputText type="text" :placeholder="currentUser.firstName" disabled />
+        <InputText type="text" :placeholder="currentUser.lastName" disabled />
+        <InputText type="text" :placeholder="currentUser.email" disabled />
+        <Button class="user-edit" type="submit" label="Edit" v-on:click.prevent="handleEditClicked"/>
+      </template>
+    </Card>
+
+  </div>
 </template>
 
 <script>
@@ -10,11 +26,15 @@ import store from "@/store/index";
   name: "UserDetails",
   components: {
 
-  }
+  },
+  props: ["currentUser"]
 })
 
 export default class UserDetails extends Vue {
 
+  handleEditClicked() {
+    this.$router.push({name: "UserEdit"})
+  }
 }
 </script>
 
