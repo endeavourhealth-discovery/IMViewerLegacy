59dfbc0fcf8650ce76e713ee99827195020f3076
Daniel Nuttal <nuttald@upco.co.uk>
2021-03-24 16:07:31 +0000

    Moved Edit/Create concept into panel options and adjusted the style of the Description/Definition scrollbars

--- front-end-vue/src/components/panels/ConceptSummary.vue
+++ front-end-vue/src/components/panels/ConceptSummary.vue
@@ -1,40 +1,42 @@
 <template>
   <div>
     <Panel header="Summary" :toggleable="true">
+      <template #icons>
+        <button class="p-panel-header-icon p-link p-mr-2" @click="toggle">
+          <span class="pi pi-cog"></span>
+        </button>
+        <Menu id="config_menu" ref="menu" :model="items" :popup="true" />
+      </template>
+
       <div class="p-grid">
         <div class="p-col-6" v-if="concept.name">
           <p><strong>Name:</strong> {{ concept.name }}</p>
           <p><strong>Iri:</strong> {{ concept.iri }}</p>
           <p><strong>Code:</strong> {{ concept.code }}</p>
+          <p><strong>Description:</strong></p>
+          <ScrollPanel style="width: 100%; height: 100px" class="custom">
+            <div>{{ concept.description }}</div>
+          </ScrollPanel>
         </div>
         <div class="p-col-6" v-if="concept.name">
           <p><strong>Status:</strong> {{ concept.status }}</p>
           <p><strong>Scheme:</strong> {{ concept.scheme.name }}</p>
           <p><strong>Type:</strong> {{ concept.conceptType }}</p>
-        </div>
-        <div class="p-col-6" v-if="concept.name">
-          <ScrollPanel style="width: 100%; height: 100px">
-            <p><strong>Description:</strong></p>
-            <div>{{ concept.description }}</div>
-          </ScrollPanel>
-        </div>
-        <div class="p-col-6" v-if="concept.name">
-          <ScrollPanel style="width: 100%; height: 100px">
-            <p><strong>Definition:</strong></p>
+          <p><strong>Definition:</strong></p>
+          <ScrollPanel style="width: 100%; height: 100px" class="custom">
             <div v-for="(item, index) in definitionDisplay" :key="item">
               {{ index !== definitionDisplay.length - 1 ? item + ";" : item }}
             </div>
           </ScrollPanel>
         </div>
-        <div class="p-col-10" v-if="concept.name"></div>
-        <div class="p-col-2" v-if="concept.name">
+        <!-- <div class="p-col-2" v-if="concept.name">
           <SplitButton
             label="Edit"
             icon="pi pi-pencil"
             @click="openEditDialog()"
             :model="items"
           ></SplitButton>
-        </div>
+        </div> -->
         <EditDialog
           @closeDialog="closeDialog"
           :display="display"
@@ -70,8 +72,24 @@ export default class ConceptSummary extends Vue {
   editDialogView = true;
   concept = {};
   definitionText = "";
-
   display = false;
+
+  items = [
+    {
+      label: "Edit Concept",
+      icon: "pi pi-pencil",
+      command: () => {
+        this.openEditDialog();
+      }
+    },
+    {
+      label: "Create New Concept",
+      icon: "pi pi-plus",
+      command: () => {
+        this.openAddDialog();
+      }
+    }
+  ];
 
   get definitionDisplay() {
     return this.definitionText.split(";");
@@ -99,14 +117,25 @@ export default class ConceptSummary extends Vue {
     this.display = true;
   }
 
-  items = [
-    {
-      label: "Create new",
-      icon: "pi pi-plus",
-      command: () => {
-        this.openAddDialog();
-      }
-    }
-  ];
+  toggle(event: any) {
+    const x = this.$refs.menu as any;
+    x.toggle(event);
+  }
 }
 </script>
+
+<style>
+.custom .p-scrollpanel-wrapper {
+  border-right: 9px solid #f4f4f4;
+}
+
+.custom .p-scrollpanel-bar {
+  background-color: #1976d2 !important;
+  opacity: 1;
+  transition: background-color 0.3s;
+}
+
+.custom .p-scrollpanel-bar:hover {
+  background-color: #135ba1 !important;
+}
+</style>
