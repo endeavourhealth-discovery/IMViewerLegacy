8ce27a8ba0073a9d43ef4d73064160073c3f9c41
matt-ergosoft <matt.hutcheson@ergosoft.co.uk>
2021-03-26 14:53:40 +0000

    created simplecount model and reportservice

---
+++ front-end-vue/src/models/reports/SimpleCount.ts
@@ -0,0 +1,15 @@
+export class SimpleCount {
+  label: string;
+  count: number;
+
+
+  constructor(
+    label: string,
+    count: number,
+  ){
+    this.label = label;
+    this.count = count;
+
+  }
+
+}
