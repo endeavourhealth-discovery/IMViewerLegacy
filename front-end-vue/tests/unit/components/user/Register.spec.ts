ca34815a7eb2bc34c619b994c70d2d9402a30c31
matt-ergosoft <matt.hutcheson@ergosoft.co.uk>
2021-03-30 12:54:48 +0100

    split register spec into 2 describes

--- front-end-vue/tests/unit/components/user/Register.spec.ts
+++ front-end-vue/tests/unit/components/user/Register.spec.ts
@@ -7,7 +7,7 @@ import InlineMessage from "primevue/inlinemessage";
 import Button from "primevue/button";
 import {PasswordStrength} from "@/models/PasswordStrength"
 
-describe("register.vue", () => {
+describe("register.vue empty", () => {
   let wrapper: any;
 
   beforeEach(() => {
@@ -27,5 +27,34 @@ describe("register.vue", () => {
     expect(wrapper.vm.showEmail1Notice).toBe(false);
     expect(wrapper.vm.showEmail2Notice).toBe(false);
     expect(wrapper.vm.showPassword2Notice).toBe(false);
+  })
+})
+
+describe("register.vue prefilled", () => {
+  let wrapper: any;
+
+  beforeEach(() => {
+    wrapper = mount(Register, {
+      global: {
+        plugins: [store],
+        components: { Card, Button, InputText, InlineMessage}
+      }
+    })
+    wrapper.vm.username = "DevTest";
+    wrapper.vm.email1 = "devtest@ergo.co.uk";
+    wrapper.vm.email2 = "devtest@ergo.co.uk";
+    wrapper.vm.password1 = "12345678";
+    wrapper.vm.password2 = "12345678";
+    wrapper.vm.firstName = "John";
+    wrapper.vm.lastName = "Doe";
+  })
+
+  it("should render data to form", async () => {
+    const userNameField = wrapper.find("#fieldUsername");
+    let userNameInput = userNameField.element as HTMLInputElement;
+    await wrapper.vm.$nextTick();
+    expect(userNameField.exists()).toBe(true);
+    expect(userNameField.element.id).toBe('fieldUsername');
+    expect(userNameInput.value).toBe("DevTest");
   })
 })
