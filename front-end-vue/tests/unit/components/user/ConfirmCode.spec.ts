03d23b23044bfd42f74f606831b4b2cc71e7868e
Daniel Nuttal <nuttald@upco.co.uk>
2021-03-30 09:23:01 +0100

    Fixed example test

--- front-end-vue/tests/unit/components/user/ConfirmCode.spec.ts
+++ front-end-vue/tests/unit/components/user/ConfirmCode.spec.ts
@@ -1,16 +1,25 @@
-import store from "@/store/index";
-import {mount } from '@vue/test-utils';
+import store from '@/store/index';
+// import { createStore } from 'vuex';
+import Card from 'primevue/card';
+import Button from 'primevue/button';
+import { mount } from '@vue/test-utils';
 import ConfirmCode from '@/components/user/ConfirmCode.vue';
 import InputText from 'primevue/inputtext';
 
-describe("ConfirmCode.vue", () => {
-    it('renders username when set', async () => {
-        store.state.registeredUsername = "testUser";
-
-        const wrapper = mount(ConfirmCode);
-        const un = wrapper.findComponent(InputText);
-        expect(un).not.toBeNull();
-        console.log(un);
-        expect(un.text()).toBe("testUser");
-    })
-});
+test('renders username when set', async () => {
+    store.commit("updateRegisteredUsername", "testUser");
+    const wrapper = mount(ConfirmCode,
+        {
+            global: {
+                plugins: [store],
+                components: { Card, Button, InputText },
+            }
+        })
+    // Assert the rendered text of the component
+    const userNameField = wrapper.find("#fieldUsername");
+    let userNameInput = userNameField.element as HTMLInputElement
+    await wrapper.vm.$nextTick();
+    expect(userNameField.exists()).toBe(true)
+    expect(userNameField.element.id).toBe('fieldUsername')
+    expect(userNameInput.value).toBe("testUser");
+})
