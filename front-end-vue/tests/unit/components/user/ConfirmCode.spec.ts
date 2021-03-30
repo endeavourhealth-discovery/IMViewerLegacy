c906d544d174083e81a4ff7fb0f12a9dfa932461
matt-ergosoft <matt.hutcheson@ergosoft.co.uk>
2021-03-30 11:46:04 +0100

    confirm code username and code tests added

--- front-end-vue/tests/unit/components/user/ConfirmCode.spec.ts
+++ front-end-vue/tests/unit/components/user/ConfirmCode.spec.ts
@@ -6,15 +6,34 @@ import { mount } from '@vue/test-utils';
 import ConfirmCode from '@/components/user/ConfirmCode.vue';
 import InputText from 'primevue/inputtext';
 
-test('renders username when set', async () => {
+let wrapper: any;
+
+it('starts empty if no store registeredUsername', async() => {
+    wrapper = mount(ConfirmCode, {
+        global: {
+            plugins: [store],
+            components: { Card, Button, InputText}
+        },
+    })
+    const userNameField = wrapper.find("#fieldUsername");
+    let userNameInput = userNameField.element as HTMLInputElement
+    await wrapper.vm.$nextTick();
+    expect(userNameField.exists()).toBe(true)
+    expect(userNameField.element.id).toBe('fieldUsername')
+    expect(userNameInput.value).toBe("");
+})
+
+beforeEach(() => {
     store.commit("updateRegisteredUsername", "testUser");
-    const wrapper = mount(ConfirmCode,
-        {
-            global: {
-                plugins: [store],
-                components: { Card, Button, InputText },
-            }
-        })
+    wrapper = mount(ConfirmCode, {
+        global: {
+            plugins: [store],
+            components: { Card, Button, InputText}
+        },
+    })
+})
+
+it('renders username when set', async () => {
     // Assert the rendered text of the component
     const userNameField = wrapper.find("#fieldUsername");
     let userNameInput = userNameField.element as HTMLInputElement
@@ -22,4 +41,23 @@ test('renders username when set', async () => {
     expect(userNameField.exists()).toBe(true)
     expect(userNameField.element.id).toBe('fieldUsername')
     expect(userNameInput.value).toBe("testUser");
+})
+
+it('renders the store registeredUsername if present', async() => {
+    const userNameField = wrapper.find("#fieldUsername");
+    let userNameInput = userNameField.element as HTMLInputElement
+    await wrapper.vm.$nextTick();
+    expect(userNameField.exists()).toBe(true)
+    expect(userNameField.element.id).toBe('fieldUsername')
+    expect(userNameInput.value).toBe("testUser");
+})
+
+it('updatesUsername when edited', async () => {
+    wrapper.vm.username = "John"
+    const userNameField = wrapper.find("#fieldUsername");
+    let userNameInput = userNameField.element as HTMLInputElement
+    await wrapper.vm.$nextTick();
+    expect(userNameField.exists()).toBe(true)
+    expect(userNameField.element.id).toBe('fieldUsername')
+    expect(userNameInput.value).toBe("John");
 })
