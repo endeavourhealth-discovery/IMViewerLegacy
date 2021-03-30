5bad663faa0aa05c99e1a3f75f131b0ab8ada5af
matt-ergosoft <matt.hutcheson@ergosoft.co.uk>
2021-03-30 15:34:01 +0100

    afinished password match tests with case sensitivity test and passed all

--- front-end-vue/tests/unit/helpers/UserMethods.spec.ts
+++ front-end-vue/tests/unit/helpers/UserMethods.spec.ts
@@ -25,4 +25,49 @@ describe("verifyIsEmail", () => {
     const email = "john@doe@gmail.com";
     expect(verifyIsEmail(email)).toBe(false);
   })
+
+  it("should fail if starts with an @", () => {
+    const email = "@gmail.com";
+    expect(verifyIsEmail(email)).toBe(false);
+  })
 })
+
+describe("verifyPasswordsMatch", () => {
+  it("should fail if passwords are empty", () => {
+    const password1 = "";
+    const password2 = "";
+    expect(verifyPasswordsMatch(password1, password2)).toBe(false);
+  })
+
+  it("should fail if either password is empty __ password1", () => {
+    const password1 = "";
+    const password2 = "12345678";
+    expect(verifyPasswordsMatch(password1, password2)).toBe(false);
+  })
+
+  it("should fail if either password is empty __ password2", () => {
+    const password1 = "12345678";
+    const password2 = "";
+    expect(verifyPasswordsMatch(password1, password2)).toBe(false);
+  })
+
+  it("should fail if passwords don't match", () => {
+    const password1 = "12345678";
+    const password2 = "12345679";
+    expect(verifyPasswordsMatch(password1, password2)).toBe(false);
+  })
+
+  it("should pass if passwords match", () => {
+    const password1 = "12345678";
+    const password2 = "12345678";
+    expect(verifyPasswordsMatch(password1, password2)).toBe(true);
+  })
+
+  it("should handle case sensitivity", () => {
+    const password1 = "passWORD";
+    const password2 = "password";
+    expect(verifyPasswordsMatch(password1, password2)).toBe(false);
+  })
+})
+
+
