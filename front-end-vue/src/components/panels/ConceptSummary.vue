be4a385905d281ca8d413469470ada2007317916
Kostas Kardatos <kostas.kardatos@answerdigital.com>
2021-04-01 14:24:21 +0100

    fix concept summary - show iris

--- front-end-vue/src/components/panels/ConceptSummary.vue
+++ front-end-vue/src/components/panels/ConceptSummary.vue
@@ -7,26 +7,48 @@
         </button>
         <Menu id="config_menu" ref="menu" :model="items" :popup="true" />
       </template>
-
       <div class="p-grid">
-        <div class="p-col-6" v-if="concept.name">
-          <p><strong>Name:</strong> {{ concept.name }}</p>
-          <p><strong>Iri:</strong> {{ concept.iri }}</p>
-          <p><strong>Code:</strong> {{ concept.code }}</p>
+        <div
+          class="p-col-6"
+          v-if="concept['http://www.w3.org/2000/01/rdf-schema#label']"
+        >
+          <p>
+            <strong>Name:</strong>
+            {{ concept["http://www.w3.org/2000/01/rdf-schema#label"] }}
+          </p>
+          <p><strong>Iri:</strong> {{ concept["@id"] }}</p>
+          <p>
+            <strong>Code:</strong>
+            {{ concept["http://endhealth.info/im#code"] }}
+          </p>
           <p><strong>Description:</strong></p>
           <ScrollPanel style="width: 100%; height: 100px" class="custom">
-            <div>{{ concept.description }}</div>
+            <div>
+              {{ concept["http://www.w3.org/2000/01/rdf-schema#comment"] }}
+            </div>
           </ScrollPanel>
         </div>
-        <div class="p-col-6" v-if="concept.name">
-          <p><strong>Status:</strong> {{ concept.status }}</p>
-          <p><strong>Scheme:</strong> {{ concept.scheme.name }}</p>
-          <p><strong>Type:</strong> {{ concept.conceptType }}</p>
+        <div
+          class="p-col-6"
+          v-if="concept['http://www.w3.org/2000/01/rdf-schema#label']"
+        >
+          <p>
+            <strong>Status:</strong>
+            {{ concept["http://endhealth.info/im#status"]["@id"] }}
+          </p>
+          <p>
+            <strong>Scheme:</strong>
+            {{ concept["http://endhealth.info/im#scheme"]["@id"] }}
+          </p>
+          <p>
+            <strong>Types:</strong>
+            {{ conceptTypes }}
+          </p>
           <p><strong>Definition:</strong></p>
-          <Definition
+          <!-- <Definition
             :definition="this.definitionText"
             style="width: 95%; height: 100px;"
-          />
+          /> -->
         </div>
         <EditDialog
           @closeDialog="closeDialog"
@@ -61,7 +83,7 @@ import Definition from "./Definition.vue";
 })
 export default class ConceptSummary extends Vue {
   editDialogView = true;
-  concept = {};
+  concept = {} as any;
   definitionText = "";
   display = false;
 
@@ -81,6 +103,16 @@ export default class ConceptSummary extends Vue {
       }
     }
   ];
+
+  get conceptTypes() {
+    const types: string[] = [];
+    this.concept["http://www.w3.org/1999/02/22-rdf-syntax-ns#type"].forEach(
+      (typeObject: any) => {
+        types.push(typeObject["@id"]);
+      }
+    );
+    return types.join();
+  }
 
   get editorConcept() {
     return this.editDialogView ? this.concept : {};
