1c43b466cc14f364b88ad4f860444ae0c1a4b473
matt-ergosoft <matt.hutcheson@ergosoft.co.uk>
2021-04-14 19:08:48 +0100

    added accreditation for icons

--- front-end-vue/src/components/user/UserEdit.vue
+++ front-end-vue/src/components/user/UserEdit.vue
@@ -12,6 +12,7 @@
             @click="toggleAvatarSelect"
           />
           <OverlayPanel ref="avatar" class="avatar-popup">
+            <div>Icons made by <a href="https://www.flaticon.com/authors/vitaly-gorbachev" title="Vitaly Gorbachev">Vitaly Gorbachev</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
             <SelectButton
               v-model="selectedAvatar"
               :options="avatarOptions"
