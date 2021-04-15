8cae0a91e132f675d3096ec577a59bcfa00d110a
matt-ergosoft <matt.hutcheson@ergosoft.co.uk>
2021-04-15 16:49:14 +0100

    added check for min search characters

--- front-end-vue/src/components/sidebar/SidebarControl.vue
+++ front-end-vue/src/components/sidebar/SidebarControl.vue
@@ -62,75 +62,79 @@ export default class SidebarControl extends Vue {
   searchTerm = "";
   active = 0;
   async search() {
-    this.active = 2;
-    const searchRequest = new SearchRequest();
-    searchRequest.termFilter = this.searchTerm;
-    searchRequest.sortBy = SortBy.Usage;
-    searchRequest.page = 1;
-    searchRequest.size = 100;
-    searchRequest.schemeFilter = store.state.filters.selectedSchemes.map(
-      s => s.iri
-    );
-    searchRequest.markIfDescendentOf = [
-      ":DiscoveryCommonDataModel",
-      ":SemanticConcept",
-      ":VSET_ValueSet"
-    ];
+    if (this.searchTerm.length > 2){
+      this.active = 2;
+      const searchRequest = new SearchRequest();
+      searchRequest.termFilter = this.searchTerm;
+      searchRequest.sortBy = SortBy.Usage;
+      searchRequest.page = 1;
+      searchRequest.size = 100;
+      searchRequest.schemeFilter = store.state.filters.selectedSchemes.map(
+        s => s.iri
+      );
+      searchRequest.markIfDescendentOf = [
+        ":DiscoveryCommonDataModel",
+        ":SemanticConcept",
+        ":VSET_ValueSet"
+      ];
 
-    searchRequest.statusFilter = [];
-    store.state.filters.selectedStatus.forEach(status => {
-      if (status == "Active") {
-        searchRequest.statusFilter.push(ConceptStatus.Active);
-      }
-      if (status == "Draft") {
-        searchRequest.statusFilter.push(ConceptStatus.Draft);
-      }
-      if (status == "Inactive") {
-        searchRequest.statusFilter.push(ConceptStatus.Inactive);
-      }
-    });
+      searchRequest.statusFilter = [];
+      store.state.filters.selectedStatus.forEach(status => {
+        if (status == "Active") {
+          searchRequest.statusFilter.push(ConceptStatus.Active);
+        }
+        if (status == "Draft") {
+          searchRequest.statusFilter.push(ConceptStatus.Draft);
+        }
+        if (status == "Inactive") {
+          searchRequest.statusFilter.push(ConceptStatus.Inactive);
+        }
+      });
 
-    searchRequest.typeFilter = [];
-    store.state.filters.selectedTypes.forEach(type => {
-      if (type == "Class") {
-        searchRequest.typeFilter.push(ConceptType.Class);
-      }
-      if (type == "ObjectProperty") {
-        searchRequest.typeFilter.push(ConceptType.ObjectProperty);
-      }
-      if (type == "DataProperty") {
-        searchRequest.typeFilter.push(ConceptType.DataProperty);
-      }
-      if (type == "DataType") {
-        searchRequest.typeFilter.push(ConceptType.DataType);
-      }
-      if (type == "Annotation") {
-        searchRequest.typeFilter.push(ConceptType.Annotation);
-      }
-      if (type == "Individual") {
-        searchRequest.typeFilter.push(ConceptType.Individual);
-      }
-      if (type == "Record") {
-        searchRequest.typeFilter.push(ConceptType.Record);
-      }
-      if (type == "ValueSet") {
-        searchRequest.typeFilter.push(ConceptType.ValueSet);
-      }
-      if (type == "Folder") {
-        searchRequest.typeFilter.push(ConceptType.Folder);
-      }
-      if (type == "Legacy") {
-        searchRequest.typeFilter.push(ConceptType.Legacy);
-      }
-    });
+      searchRequest.typeFilter = [];
+      store.state.filters.selectedTypes.forEach(type => {
+        if (type == "Class") {
+          searchRequest.typeFilter.push(ConceptType.Class);
+        }
+        if (type == "ObjectProperty") {
+          searchRequest.typeFilter.push(ConceptType.ObjectProperty);
+        }
+        if (type == "DataProperty") {
+          searchRequest.typeFilter.push(ConceptType.DataProperty);
+        }
+        if (type == "DataType") {
+          searchRequest.typeFilter.push(ConceptType.DataType);
+        }
+        if (type == "Annotation") {
+          searchRequest.typeFilter.push(ConceptType.Annotation);
+        }
+        if (type == "Individual") {
+          searchRequest.typeFilter.push(ConceptType.Individual);
+        }
+        if (type == "Record") {
+          searchRequest.typeFilter.push(ConceptType.Record);
+        }
+        if (type == "ValueSet") {
+          searchRequest.typeFilter.push(ConceptType.ValueSet);
+        }
+        if (type == "Folder") {
+          searchRequest.typeFilter.push(ConceptType.Folder);
+        }
+        if (type == "Legacy") {
+          searchRequest.typeFilter.push(ConceptType.Legacy);
+        }
+      });
 
-    store.dispatch("fetchSearchResults", searchRequest).then(res => {
-      if (!res) {
-        this.$toast.add(
-          LoggerService.error("Search results server request failed")
-        );
-      }
-    });
+      store.dispatch("fetchSearchResults", searchRequest).then(res => {
+        if (!res) {
+          this.$toast.add(
+            LoggerService.error("Search results server request failed")
+          );
+        }
+      });
+    } else {
+      this.active = 0;
+    }
   }
 }
 </script>
