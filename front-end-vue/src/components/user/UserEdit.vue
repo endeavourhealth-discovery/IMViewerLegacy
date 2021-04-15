7c41113e80e43a91cb107f30ff295970bc94e397
matt-ergosoft <matt.hutcheson@ergosoft.co.uk>
2021-04-15 13:44:24 +0100

    changed UserEdit to use AvatarWithSelector component

--- front-end-vue/src/components/user/UserEdit.vue
+++ front-end-vue/src/components/user/UserEdit.vue
@@ -4,47 +4,7 @@
       class="p-d-flex p-flex-column p-jc-sm-around p-ai-center user-edit-card"
     >
       <template #header>
-        <div class="avatar-container">
-          <img
-            id="selected-avatar"
-            :src="getUrl(selectedAvatar.value)"
-            alt="avatar icon"
-          />
-          <Button
-            icon="pi pi-angle-down"
-            class="p-button-rounded p-button-primary avatar-button"
-            @click="toggleAvatarSelect"
-          />
-          <OverlayPanel ref="avatar" class="avatar-popup">
-            <div>
-              Icons made by
-              <a
-                href="https://www.flaticon.com/authors/vitaly-gorbachev"
-                title="Vitaly Gorbachev"
-              >
-                Vitaly Gorbachev
-              </a>
-              from
-              <a href="https://www.flaticon.com/" title="Flaticon">
-                www.flaticon.com
-              </a>
-            </div>
-            <SelectButton
-              v-model="selectedAvatar"
-              :options="avatarOptions"
-              dataKey="value"
-            >
-              <template #option="slotProps">
-                <img
-                  class="avatar-select"
-                  :src="require('@/assets/avatars/' + slotProps.option.value)"
-                  alt="avatar icon"
-                  style="width: 3em;"
-                />
-              </template>
-            </SelectButton>
-          </OverlayPanel>
-        </div>
+        <avatar-with-selector :selectedAvatar="selectedAvatar" @avatarSelected="updateAvatar" />
       </template>
       <template #title>
         Edit My Account
@@ -223,10 +183,13 @@ import {
 import { PasswordStrength } from "@/models/user/PasswordStrength";
 import AuthService from "@/services/AuthService";
 import { avatars } from "@/models/user/Avatars";
+import AvatarWithSelector from "./AvatarWithSelector.vue";
 
 @Options({
   name: "UserEdit",
-  components: {},
+  components: {
+    "avatar-with-selector": AvatarWithSelector
+  },
   computed: mapState(["currentUser", "isLoggedIn"]),
   watch: {
     email1: {
@@ -475,13 +438,8 @@ export default class UserEdit extends Vue {
     });
   }
 
-  toggleAvatarSelect(event: any) {
-    const x = this.$refs.avatar as any;
-    x.toggle(event);
-  }
-
-  getUrl(item: string) {
-    return require("@/assets/avatars/" + item);
+  updateAvatar(newValue: { value: string }) {
+    this.selectedAvatar = newValue;
   }
 }
 </script>
@@ -499,24 +457,5 @@ export default class UserEdit extends Vue {
 
 .user-edit-card {
   padding: 0 2em;
-}
-
-.avatar-container {
-  position: relative;
-  padding: 1.5em;
-  /* margin: 1em; */
-}
-
-.avatar-button {
-  position: absolute;
-  bottom: 0;
-  right: 0;
-}
-
-#selected-avatar {
-  margin-block-start: 0.5em;
-  width: 150px;
-  border: 1px solid lightgray;
-  border-radius: 50%;
 }
 </style>
