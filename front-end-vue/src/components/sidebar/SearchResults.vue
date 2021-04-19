fc59a357a879072eedd4cf9d9e5044e8ff4df582
Richard Collier <gitkraken@tirinoarim.co.uk>
2021-04-19 13:58:19 +0100

    Enhanced search results to include match plus name.

--- front-end-vue/src/components/sidebar/SearchResults.vue
+++ front-end-vue/src/components/sidebar/SearchResults.vue
@@ -25,26 +25,25 @@
       currentPageReportTemplate="Showing {first} to {last} of {totalRecords}"
       :rows="15"
     >
-      <template #header> Results </template>
-      <!-- <Column field="name" header="Name"></Column> -->
-
-      <Column field="name" header="Name">
+      <Column field="name" header="Results">
         <template #body="slotProps">
           <div
             @mouseenter="toggle($event, slotProps.data)"
             @mouseleave="toggle($event, slotProps.data)"
-          >
+          style="display: flex">
+            <div style="left: 0; width: 3rem; height: 2rem;">
             <i
               :class="getPerspectiveByConceptType(slotProps.data.conceptType)"
-              style="fontSize: 2rem; color: lightgrey; padding: 5px;"
+              style="fontSize: 2.5rem; color: lightgrey; padding: 5px;"
             />
-            {{ slotProps.data.name }}
+            </div>
+            <div style="right: 0; height: 2.5rem;">
+              {{ slotProps.data.match }}<br>
+              <small style="color:lightgrey">{{ slotProps.data.name }}</small>
+            </div>
           </div>
         </template>
       </Column>
-
-      <!-- <Column field="conceptType" header="Type" :sortable="true"></Column>
-            <Column field="scheme.name" header="Scheme" :sortable="true"></Column> -->
     </DataTable>
 
     <OverlayPanel
