86d38d97eda430b20e14fceb9beabc0c3610ffa6
matt-ergosoft <matt.hutcheson@ergosoft.co.uk>
2021-04-27 13:13:43 +0100

    swapped sonceptsummary to use Concept, and synonym to use type

--- front-end-vue/src/models/TTConcept/Concept.ts
+++ front-end-vue/src/models/TTConcept/Concept.ts
@@ -9,4 +9,5 @@ export interface Concept {
   "http://www.w3.org/1999/02/22-rdf-syntax-ns#type": [];
   "http://www.w3.org/2000/01/rdf-schema#label": string;
   "http://www.w3.org/2002/07/owl#equivalentClass": [];
+  "http://www.w3.org/2000/01/rdf-schema#comment": string;
 }
