c779c662ef0c9fc8226299e46d6c6a7e8ab9741d
matt-ergosoft <matt.hutcheson@ergosoft.co.uk>
2021-04-29 11:55:40 +0100

    removed console.log

--- front-end-vue/src/components/dashboard/OntologyOverview.vue
+++ front-end-vue/src/components/dashboard/OntologyOverview.vue
@@ -56,7 +56,6 @@ export default class OntologyOverview extends Vue {
     } else {
       ReportService.getConceptCategoryReport()
         .then(res => {
-          console.log(res.data)
           this.tableData = res.data;
           store.commit("updateOntologyOverview", this.tableData);
           store.commit("updateLoading", {
