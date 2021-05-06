ccd98ecda8e8043203c9210c4f76a3fb65b2c540
matt-ergosoft <matt.hutcheson@ergosoft.co.uk>
2021-05-06 10:03:39 +0100

    added test for home and passed

---
+++ front-end-vue/tests/unit/views/Home.spec.ts
@@ -0,0 +1,25 @@
+import store from "@/store/index";
+import { shallowMount } from "@vue/test-utils";
+import Home from "@/views/Home.vue";
+import Header from "@/components/home/Header.vue";
+import SidebarControl from "@/components/sidebar/SidebarControl.vue";
+import SideNav from "@/components/home/SideNav.vue";
+
+describe("User.vue", () => {
+  let wrapper: any;
+
+  beforeEach(() => {
+    wrapper = shallowMount(Home, {
+      global: {
+        plugins: [store],
+        components: { Header, SidebarControl, SideNav },
+        stubs: ["router-link", "router-view"]
+      },
+    });
+  });
+
+  it("should render containers", () => {
+    expect(wrapper.find(".layout-main")).toBeTruthy();
+    expect(wrapper.find(".main-grid")).toBeTruthy();
+  })
+});
