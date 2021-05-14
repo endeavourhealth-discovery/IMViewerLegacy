c813226e958298d23474d6bb0691051372d65eb1
matt-ergosoft <matt.hutcheson@ergosoft.co.uk>
2021-05-14 16:39:49 +0100

    finished tests for authService

--- front-end-vue/tests/unit/services/AuthService.spec.ts
+++ front-end-vue/tests/unit/services/AuthService.spec.ts
@@ -311,61 +311,221 @@ describe("updateUser", () => {
     expect(Auth.updateUserAttributes).toHaveBeenCalledTimes(0);
     expect(promiseResult).toStrictEqual(new CustomAlert(403, "Authentication error with server", err));
   });
+});
 
-  describe("changePassword", () => {
-    beforeEach(() => {
-      jest.clearAllMocks();
-    });
-
-    it("returns 200 with auth success", async () => {
-      Auth.currentAuthenticatedUser = jest.fn().mockResolvedValueOnce({
-        username: "devtest",
-        attributes: {
-          "custom:avatar": "colour/002-man.png",
-          "custom:forename": "John",
-          "custom:surname": "Doe",
-          email: "john.doe@ergosoft.co.uk",
-          email_verified: true,
-          sub: "9gkej864-l39k-9u87-4lau-w7777b3m5g09"
-        }
-      });
-      Auth.changePassword = jest.fn().mockResolvedValue( { code: 200 });
-      const result = AuthService.changePassword("12345678", "87654321");
-      let promiseResult: any;
-      result.then(res => {
-        promiseResult = res
-      })
-      await flushPromises();
-      expect(Auth.currentAuthenticatedUser).toHaveBeenCalledTimes(1);
-      expect(Auth.changePassword).toHaveBeenCalledTimes(1);
-      expect(promiseResult).toStrictEqual(new CustomAlert(200, "Password successfully changed"));
-    });
-
-    it("returns 400 with auth fail", async () => {
-      Auth.currentAuthenticatedUser = jest.fn().mockResolvedValueOnce({
-        username: "devtest",
-        attributes: {
-          "custom:avatar": "colour/002-man.png",
-          "custom:forename": "John",
-          "custom:surname": "Doe",
-          email: "john.doe@ergosoft.co.uk",
-          email_verified: true,
-          sub: "9gkej864-l39k-9u87-4lau-w7777b3m5g09"
-        }
-      });
-      Auth.changePassword = jest.fn().mockRejectedValue( { code: "passwordChangeError", message: "Password change error" });
-      const result = AuthService.changePassword("12345678", "87654321");
-      let promiseResult: any;
-      let err: any;
-      result.then(res => {
-        err = res.error
-        promiseResult = res
-      });
-      await flushPromises();
-      expect(Auth.currentAuthenticatedUser).toHaveBeenCalledTimes(1);
-      expect(Auth.changePassword).toHaveBeenCalledTimes(1);
-      expect(promiseResult).toStrictEqual(new CustomAlert(400, "Password change error", err));
-    });
+describe("changePassword", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
   });
 
+  it("returns 200 with auth success", async () => {
+    Auth.currentAuthenticatedUser = jest.fn().mockResolvedValueOnce({
+      username: "devtest",
+      attributes: {
+        "custom:avatar": "colour/002-man.png",
+        "custom:forename": "John",
+        "custom:surname": "Doe",
+        email: "john.doe@ergosoft.co.uk",
+        email_verified: true,
+        sub: "9gkej864-l39k-9u87-4lau-w7777b3m5g09"
+      }
+    });
+    Auth.changePassword = jest.fn().mockResolvedValue( { code: 200 });
+    const result = AuthService.changePassword("12345678", "87654321");
+    let promiseResult: any;
+    result.then(res => {
+      promiseResult = res
+    })
+    await flushPromises();
+    expect(Auth.currentAuthenticatedUser).toHaveBeenCalledTimes(1);
+    expect(Auth.changePassword).toHaveBeenCalledTimes(1);
+    expect(promiseResult).toStrictEqual(new CustomAlert(200, "Password successfully changed"));
+  });
+
+  it("returns 400 with auth fail", async () => {
+    Auth.currentAuthenticatedUser = jest.fn().mockResolvedValueOnce({
+      username: "devtest",
+      attributes: {
+        "custom:avatar": "colour/002-man.png",
+        "custom:forename": "John",
+        "custom:surname": "Doe",
+        email: "john.doe@ergosoft.co.uk",
+        email_verified: true,
+        sub: "9gkej864-l39k-9u87-4lau-w7777b3m5g09"
+      }
+    });
+    Auth.changePassword = jest.fn().mockRejectedValue( { code: "passwordChangeError", message: "Password change error" });
+    const result = AuthService.changePassword("12345678", "87654321");
+    let promiseResult: any;
+    let err: any;
+    result.then(res => {
+      err = res.error
+      promiseResult = res
+    });
+    await flushPromises();
+    expect(Auth.currentAuthenticatedUser).toHaveBeenCalledTimes(1);
+    expect(Auth.changePassword).toHaveBeenCalledTimes(1);
+    expect(promiseResult).toStrictEqual(new CustomAlert(400, "Password change error", err));
+  });
+});
+
+describe("forgotPassword", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("returns 200 with auth success", async () => {
+    Auth.forgotPassword = jest.fn().mockResolvedValue( { code: 200 });
+    const result = AuthService.forgotPassword("devtest");
+    let promiseResult: any;
+    result.then(res => {
+      promiseResult = res
+    })
+    await flushPromises();
+    expect(Auth.forgotPassword).toHaveBeenCalledTimes(1);
+    expect(Auth.forgotPassword).toHaveBeenCalledWith("devtest");
+    expect(promiseResult).toStrictEqual(new CustomAlert(200, "Password reset request sent to server"));
+  });
+
+  it("returns 400 with auth fail ___ fail", async () => {
+    Auth.forgotPassword = jest.fn().mockRejectedValue( { code: "PasswordResetFail", message: "Password reset error" });
+    const result = AuthService.forgotPassword("devtest");
+    let promiseResult: any;
+    let err: any;
+    result.then(res => {
+      err = res.error
+      promiseResult = res
+    });
+    await flushPromises();
+    expect(Auth.forgotPassword).toHaveBeenCalledTimes(1);
+    expect(Auth.forgotPassword).toHaveBeenCalledWith("devtest");
+    expect(promiseResult).toStrictEqual(new CustomAlert(400, "Error resetting password from server", err));
+  });
+});
+
+describe("forgotPasswordSubmit", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("returns 200 with auth success", async () => {
+    Auth.forgotPasswordSubmit = jest.fn().mockResolvedValue( { code: 200 });
+    const result = AuthService.forgotPasswordSubmit("devtest", "123456", "87654321");
+    let promiseResult: any;
+    result.then(res => {
+      promiseResult = res
+    })
+    await flushPromises();
+    expect(Auth.forgotPasswordSubmit).toHaveBeenCalledTimes(1);
+    expect(Auth.forgotPasswordSubmit).toHaveBeenCalledWith("devtest", "123456", "87654321");
+    expect(promiseResult).toStrictEqual(new CustomAlert(200, "Password reset successfully"));
+  });
+
+  it("returns 400 with auth fail ___ fail", async () => {
+    Auth.forgotPasswordSubmit = jest.fn().mockRejectedValue( { code: "PasswordResetCodeFail", message: "Password reset code error" });
+    const result = AuthService.forgotPasswordSubmit("devtest", "123456", "87654321");
+    let promiseResult: any;
+    let err: any;
+    result.then(res => {
+      err = res.error
+      promiseResult = res
+    });
+    await flushPromises();
+    expect(Auth.forgotPasswordSubmit).toHaveBeenCalledTimes(1);
+    expect(Auth.forgotPasswordSubmit).toHaveBeenCalledWith("devtest", "123456", "87654321");
+    expect(promiseResult).toStrictEqual(new CustomAlert(400, "Error submitting password-reset credentials", err));
+  });
+
+  it("returns 403 with auth fail ___ expired", async () => {
+    Auth.forgotPasswordSubmit = jest.fn().mockRejectedValue( { code: "ExpiredCodeException", message: "Password reset error" });
+    const result = AuthService.forgotPasswordSubmit("devtest", "123456", "87654321");
+    let promiseResult: any;
+    let err: any;
+    result.then(res => {
+      err = res.error
+      promiseResult = res
+    });
+    await flushPromises();
+    expect(Auth.forgotPasswordSubmit).toHaveBeenCalledTimes(1);
+    expect(Auth.forgotPasswordSubmit).toHaveBeenCalledWith("devtest", "123456", "87654321");
+    expect(promiseResult).toStrictEqual(new CustomAlert(403, "Code has expired", err));
+  });
+});
+
+describe("forgotUsername", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("returns 200 with auth success", async () => {
+    Auth.verifyCurrentUserAttribute = jest.fn().mockResolvedValue( { code: 200 });
+    const result = AuthService.forgotUsername("john.doe@ergosoft.co.uk");
+    let promiseResult: any;
+    result.then(res => {
+      promiseResult = res
+    })
+    await flushPromises();
+    expect(Auth.verifyCurrentUserAttribute).toHaveBeenCalledTimes(1);
+    expect(Auth.verifyCurrentUserAttribute).toHaveBeenCalledWith("john.doe@ergosoft.co.uk");
+    expect(promiseResult).toStrictEqual(new CustomAlert(200, "Account recovery code sent"));
+  });
+
+  it("returns 400 with auth fail ___ fail", async () => {
+    Auth.verifyCurrentUserAttribute = jest.fn().mockRejectedValue( { code: "UsernameRecoveryFail", message: "Username recovery error" });
+    const result = AuthService.forgotUsername("john.doe@ergosoft.co.uk");
+    let promiseResult: any;
+    let err: any;
+    result.then(res => {
+      err = res.error
+      promiseResult = res
+    });
+    await flushPromises();
+    expect(Auth.verifyCurrentUserAttribute).toHaveBeenCalledTimes(1);
+    expect(Auth.verifyCurrentUserAttribute).toHaveBeenCalledWith("john.doe@ergosoft.co.uk");
+    expect(promiseResult).toStrictEqual(new CustomAlert(400, "Error submitting email", err));
+  });
+});
+
+describe("getCurrentAuthenticatedUser", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("returns 200 with auth success, user, password empty, with id", async () => {
+    Auth.currentAuthenticatedUser = jest.fn().mockResolvedValueOnce({
+      username: "devtest",
+      attributes: {
+        "custom:avatar": "colour/002-man.png",
+        "custom:forename": "John",
+        "custom:surname": "Doe",
+        email: "john.doe@ergosoft.co.uk",
+        email_verified: true,
+        sub: "9gkej864-l39k-9u87-4lau-w7777b3m5g09"
+      }
+    });
+    const result = AuthService.getCurrentAuthenticatedUser();
+    let promiseResult: any;
+    result.then(res => {
+      promiseResult = res
+    })
+    await flushPromises();
+    const currentUser = new User("devtest", "John", "Doe", "john.doe@ergosoft.co.uk", "", { value: "colour/002-man.png" });
+    currentUser.setId("9gkej864-l39k-9u87-4lau-w7777b3m5g09");
+    expect(Auth.currentAuthenticatedUser).toHaveBeenCalledTimes(1);
+    expect(promiseResult).toStrictEqual(new CustomAlert(200, "User authenticated successfully", undefined, currentUser));
+  });
+
+  it("returns 400 with auth fail", async () => {
+    Auth.currentAuthenticatedUser = jest.fn().mockRejectedValue({ code: "currentUserFail", message: "get current user error" });
+    const result = AuthService.getCurrentAuthenticatedUser();
+    let promiseResult: any;
+    let err: any;
+    result.then(res => {
+      err = res.error
+      promiseResult = res
+    });
+    await flushPromises();
+    expect(Auth.currentAuthenticatedUser).toHaveBeenCalledTimes(1);
+    expect(promiseResult).toStrictEqual(new CustomAlert(403, "Error authenticating current user", err));
+  });
 });
