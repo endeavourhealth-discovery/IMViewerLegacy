af306d0f637e2c4dd96faeb6ba0360ff142d596e
matt-ergosoft <matt.hutcheson@ergosoft.co.uk>
2021-05-17 12:41:58 +0100

    added tests for login handlesubmit and checkKey. Also removed redundant if from login component

--- front-end-vue/tests/unit/components/user/Login.spec.ts
+++ front-end-vue/tests/unit/components/user/Login.spec.ts
@@ -11,7 +11,7 @@ const testUser = new User("devtest", "John", "Doe", "john.doe@ergosoft.co.uk", "
 
 testUser.setId("9gkej864-l39k-9u87-4lau-w7777b3m5g09")
 
-AuthService.signIn = jest.fn().mockResolvedValue({ status: 200, message: "Password reset successful", user: testUser });
+AuthService.signIn = jest.fn().mockResolvedValue({ status: 200, message: "Login successful", user: testUser });
 
 Swal.fire = jest.fn().mockImplementation(() => Promise.resolve({ isConfirmed: true }));
 
@@ -102,5 +102,103 @@ describe("login.vue with registeredUser", () => {
       ["updateRegisteredUsername", null],
       ["updateIsLoggedIn", true]
     ]);
+  });
+
+  it("checks for existing avatar and sets user avatar ___ true", async() => {
+    wrapper.vm.username = "Devtest";
+    wrapper.vm.password = "12345678";
+    wrapper.vm.handleSubmit();
+    await wrapper.vm.$nextTick();
+    await flushPromises();
+    expect(mockStore.commit).toBeCalledWith("updateCurrentUser", testUser);
+  });
+
+  it("checks for existing avatar and sets user avatar ___ false", async() => {
+    const legacyUser = { username: "legacy", avatar: { value: "image.jpg" } };
+    AuthService.signIn = jest.fn().mockResolvedValue({ status: 200, message: "Login successful", user: legacyUser });
+    wrapper.vm.username = "Devtest";
+    wrapper.vm.password = "12345678";
+    wrapper.vm.handleSubmit();
+    await wrapper.vm.$nextTick();
+    await flushPromises();
+    expect(mockStore.commit).toHaveBeenNthCalledWith(1,"updateCurrentUser", { username: "legacy", avatar: { value: "colour/001-man.png" } });
+  });
+
+  it("fires swal on auth success", async() => {
+    wrapper.vm.username = "Devtest";
+    wrapper.vm.password = "12345678";
+    wrapper.vm.handleSubmit();
+    await wrapper.vm.$nextTick();
+    await flushPromises();
+    expect(Swal.fire).toBeCalledTimes(1);
+    expect(Swal.fire).toBeCalledWith({ icon: "success", title: "Success", text: "Login successful" });
+  });
+
+  it("reroutes on auth success", async() => {
+    wrapper.vm.username = "Devtest";
+    wrapper.vm.password = "12345678";
+    wrapper.vm.handleSubmit();
+    await wrapper.vm.$nextTick();
+    await flushPromises();
+    expect(mockRouter.push).toBeCalledTimes(1);
+    expect(mockRouter.push).toBeCalledWith({ name: "Home" });
+  });
+
+  it("fires swal on auth error ___ 401", async() => {
+    AuthService.signIn = jest.fn().mockResolvedValue({ status: 401, message: "Login successful", user: testUser });
+    wrapper.vm.username = "Devtest";
+    wrapper.vm.password = "12345678";
+    wrapper.vm.handleSubmit();
+    await wrapper.vm.$nextTick();
+    await flushPromises();
+    expect(Swal.fire).toBeCalledTimes(1);
+    expect(Swal.fire).toBeCalledWith({ icon: "warning", title: "User Unconfirmed", text: "Account has not been confirmed. Please confirm account to continue.", showCloseButton: true, showCancelButton: true, confirmButtonText: "Confirm Account" });
+  });
+
+  it("updates store and reroutes on auth error ___ 401", async() => {
+    AuthService.signIn = jest.fn().mockResolvedValue({ status: 401, message: "Login successful", user: testUser });
+    wrapper.vm.username = "Devtest";
+    wrapper.vm.password = "12345678";
+    wrapper.vm.handleSubmit();
+    await wrapper.vm.$nextTick();
+    await flushPromises();
+    expect(mockStore.commit).toBeCalledTimes(1);
+    expect(mockStore.commit).toBeCalledWith("updateRegisteredUsername", "Devtest");
+    expect(mockRouter.push).toBeCalledTimes(1);
+    expect(mockRouter.push).toBeCalledWith({ name: "ConfirmCode" });
+  });
+
+  it("fires swal on auth error ___ other", async() => {
+    AuthService.signIn = jest.fn().mockResolvedValue({ status: 400, message: "Login failed", user: testUser });
+    wrapper.vm.username = "Devtest";
+    wrapper.vm.password = "12345678";
+    wrapper.vm.handleSubmit();
+    await wrapper.vm.$nextTick();
+    await flushPromises();
+    expect(Swal.fire).toBeCalledTimes(1);
+    expect(Swal.fire).toBeCalledWith({ icon: "error", title: "Error", text: "Login failed", confirmButtonText: "Close" });
+  });
+
+  it("fires swal on authservice error", async() => {
+    AuthService.signIn = jest.fn().mockRejectedValue({ status: 400, message: "Login failed", error: "deliberate test error" });
+    wrapper.vm.username = "Devtest";
+    wrapper.vm.password = "12345678";
+    wrapper.vm.handleSubmit();
+    await wrapper.vm.$nextTick();
+    await flushPromises();
+    expect(Swal.fire).toBeCalledTimes(1);
+    expect(Swal.fire).toBeCalledWith({ icon: "error", title: "Error", text: "Authentication error", confirmButtonText: "Close" });
+  });
+
+  it("can check a keycode ___ correct", async() => {
+    wrapper.vm.checkKey({ keyCode: 13 });
+    await wrapper.vm.$nextTick();
+    expect(AuthService.signIn).toBeCalledTimes(1);
+  });
+
+  it("can check a keycode ___ incorrect", async() => {
+    wrapper.vm.checkKey({ keyCode: 12 });
+    await wrapper.vm.$nextTick();
+    expect(AuthService.signIn).toBeCalledTimes(0);
   });
 });
