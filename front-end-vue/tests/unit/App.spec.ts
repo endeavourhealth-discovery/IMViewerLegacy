fc3afe4a714e652b44c80606cc2111fcf3ef1f12
matt-ergosoft <matt.hutcheson@ergosoft.co.uk>
2021-05-18 11:18:42 +0100

    swapped app.vue to vue3 typescript. Updated app.spec to new format

--- front-end-vue/tests/unit/App.spec.ts
+++ front-end-vue/tests/unit/App.spec.ts
@@ -1,22 +1,26 @@
-import store from "@/store/index";
 import { shallowMount } from "@vue/test-utils";
 import App from "@/App.vue";
 import Toast from "primevue/toast";
 
 describe("App.vue", () => {
   let wrapper: any;
+  let mockStore: any;
 
   beforeEach(() => {
+    mockStore = {
+      state: {historyCount: 1},
+      commit: jest.fn()
+    }
     wrapper = shallowMount(App, {
       global: {
-        plugins: [store],
         components: { Toast },
-        stubs: ["router-link", "router-view"]
+        stubs: ["router-link", "router-view"],
+        mocks: { $store: mockStore }
       },
     });
   });
 
   it("should update store history count on mount", () => {
-    expect(store.state.historyCount).toBeGreaterThan(0);
+    expect(mockStore.commit).toBeCalledTimes(1);
   })
 });
