d82bc3bfb246e5a357576fa84c68e5f71a2ec970
matt-ergosoft <matt.hutcheson@ergosoft.co.uk>
2021-06-08 11:23:07 +0100

    fixed usedIn to have correct height for listBox content

--- front-end-vue/src/components/concept/Terms.vue
+++ front-end-vue/src/components/concept/Terms.vue
@@ -18,8 +18,8 @@
     id="terms-table"
   >
     <Column field="scheme.name" header="Scheme"></Column>
-    <Column field="term" header="Term" style="flex: 0 0 75%"></Column>
-    <Column field="code" header="Code" style="flex: 0 0 25%; word-break: break-all;"></Column>
+    <Column field="term" header="Term" style="flex: 0 0 65%"></Column>
+    <Column field="code" header="Code" style="flex: 0 0 35%; word-break: break-all;"></Column>
     <template #groupheader="slotProps">
       <span style="font-weight: 700; color:rgba(51,153,255,0.8)">
         Scheme : {{ slotProps.data.scheme.name }}
