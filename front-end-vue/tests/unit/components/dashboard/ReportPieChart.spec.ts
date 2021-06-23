10093c98e46644e1be3ff59ef036a8294e1911d4
Richard Collier <gitkraken@tirinoarim.co.uk>
2021-06-23 16:25:35 +0100

    Fix for inferred json types

--- front-end-vue/tests/unit/components/dashboard/ReportPieChart.spec.ts
+++ front-end-vue/tests/unit/components/dashboard/ReportPieChart.spec.ts
@@ -19,163 +19,60 @@ describe("ReportPieChart.vue", () => {
       data: {
           "@id": "http://endhealth.info/im#ontologyConceptTypes",
           "http://endhealth.info/im#hasStatsReportEntry": [{
-              "http://www.w3.org/2002/07/owl#hasValue": {
-                  "@value": "1030354",
-                  "@type": "http://www.w3.org/2001/XMLSchema#string"
-              },
-              "http://www.w3.org/2000/01/rdf-schema#label": {
-                  "@value": "Class",
-                  "@type": "http://www.w3.org/2001/XMLSchema#string"
-              }
+              "http://www.w3.org/2002/07/owl#hasValue": 1030354,
+              "http://www.w3.org/2000/01/rdf-schema#label": "Class"
           }, {
-              "http://www.w3.org/2002/07/owl#hasValue": {
-                  "@value": "93282",
-                  "@type": "http://www.w3.org/2001/XMLSchema#string"
-              },
-              "http://www.w3.org/2000/01/rdf-schema#label": {
-                  "@value": "Legacy concept",
-                  "@type": "http://www.w3.org/2001/XMLSchema#string"
-              }
+              "http://www.w3.org/2002/07/owl#hasValue": 93282,
+              "http://www.w3.org/2000/01/rdf-schema#label": "Legacy concept"
           }, {
-              "http://www.w3.org/2002/07/owl#hasValue": {
-                  "@value": "1811",
-                  "@type": "http://www.w3.org/2001/XMLSchema#string"
-              },
-              "http://www.w3.org/2000/01/rdf-schema#label": {
-                  "@value": "Object property",
-                  "@type": "http://www.w3.org/2001/XMLSchema#string"
-              }
+              "http://www.w3.org/2002/07/owl#hasValue": 1811,
+              "http://www.w3.org/2000/01/rdf-schema#label": "Object property"
           }, {
-              "http://www.w3.org/2002/07/owl#hasValue": {
-                  "@value": "1122",
-                  "@type": "http://www.w3.org/2001/XMLSchema#string"
-              },
-              "http://www.w3.org/2000/01/rdf-schema#label": {
-                  "@value": "Set",
-                  "@type": "http://www.w3.org/2001/XMLSchema#string"
-              }
+              "http://www.w3.org/2002/07/owl#hasValue": 1122,
+              "http://www.w3.org/2000/01/rdf-schema#label": "Set"
           }, {
-              "http://www.w3.org/2002/07/owl#hasValue": {
-                  "@value": "99",
-                  "@type": "http://www.w3.org/2001/XMLSchema#string"
-              },
-              "http://www.w3.org/2000/01/rdf-schema#label": {
-                  "@value": "Node shape",
-                  "@type": "http://www.w3.org/2001/XMLSchema#string"
-              }
+              "http://www.w3.org/2002/07/owl#hasValue": 99,
+              "http://www.w3.org/2000/01/rdf-schema#label": "Node shape"
           }, {
-              "http://www.w3.org/2002/07/owl#hasValue": {
-                  "@value": "94",
-                  "@type": "http://www.w3.org/2001/XMLSchema#string"
-              },
-              "http://www.w3.org/2000/01/rdf-schema#label": {
-                  "@value": "Record type",
-                  "@type": "http://www.w3.org/2001/XMLSchema#string"
-              }
+              "http://www.w3.org/2002/07/owl#hasValue": 94,
+              "http://www.w3.org/2000/01/rdf-schema#label": "Record type"
           }, {
-              "http://www.w3.org/2002/07/owl#hasValue": {
-                  "@value": "68",
-                  "@type": "http://www.w3.org/2001/XMLSchema#string"
-              },
-              "http://www.w3.org/2000/01/rdf-schema#label": {
-                  "@value": "Data property",
-                  "@type": "http://www.w3.org/2001/XMLSchema#string"
-              }
+              "http://www.w3.org/2002/07/owl#hasValue": 68,
+              "http://www.w3.org/2000/01/rdf-schema#label": "Data property"
           }, {
-              "http://www.w3.org/2002/07/owl#hasValue": {
-                  "@value": "45",
-                  "@type": "http://www.w3.org/2001/XMLSchema#string"
-              },
-              "http://www.w3.org/2000/01/rdf-schema#label": {
-                  "@value": "Query set",
-                  "@type": "http://www.w3.org/2001/XMLSchema#string"
-              }
+              "http://www.w3.org/2002/07/owl#hasValue": 45,
+              "http://www.w3.org/2000/01/rdf-schema#label": "Query set"
           }, {
-              "http://www.w3.org/2002/07/owl#hasValue": {
-                  "@value": "26",
-                  "@type": "http://www.w3.org/2001/XMLSchema#string"
-              },
-              "http://www.w3.org/2000/01/rdf-schema#label": {
-                  "@value": "Functional property",
-                  "@type": "http://www.w3.org/2001/XMLSchema#string"
-              }
+              "http://www.w3.org/2002/07/owl#hasValue": 26,
+              "http://www.w3.org/2000/01/rdf-schema#label": "Functional property"
           }, {
-              "http://www.w3.org/2002/07/owl#hasValue": {
-                  "@value": "23",
-                  "@type": "http://www.w3.org/2001/XMLSchema#string"
-              },
-              "http://www.w3.org/2000/01/rdf-schema#label": {
-                  "@value": "Annotation property",
-                  "@type": "http://www.w3.org/2001/XMLSchema#string"
-              }
+              "http://www.w3.org/2002/07/owl#hasValue": 23,
+              "http://www.w3.org/2000/01/rdf-schema#label": "Annotation property"
           }, {
-              "http://www.w3.org/2002/07/owl#hasValue": {
-                  "@value": "11",
-                  "@type": "http://www.w3.org/2001/XMLSchema#string"
-              },
-              "http://www.w3.org/2000/01/rdf-schema#label": {
-                  "@value": "Symmetric property",
-                  "@type": "http://www.w3.org/2001/XMLSchema#string"
-              }
+              "http://www.w3.org/2002/07/owl#hasValue": 11,
+              "http://www.w3.org/2000/01/rdf-schema#label": "Symmetric property"
           }, {
-              "http://www.w3.org/2002/07/owl#hasValue": {
-                  "@value": "11",
-                  "@type": "http://www.w3.org/2001/XMLSchema#string"
-              },
-              "http://www.w3.org/2000/01/rdf-schema#label": {
-                  "@value": "Transitive property",
-                  "@type": "http://www.w3.org/2001/XMLSchema#string"
-              }
+              "http://www.w3.org/2002/07/owl#hasValue": 11,
+              "http://www.w3.org/2000/01/rdf-schema#label": "Transitive property"
           }, {
-              "http://www.w3.org/2002/07/owl#hasValue": {
-                  "@value": "8",
-                  "@type": "http://www.w3.org/2001/XMLSchema#string"
-              },
-              "http://www.w3.org/2000/01/rdf-schema#label": {
-                  "@value": "Folder",
-                  "@type": "http://www.w3.org/2001/XMLSchema#string"
-              }
+              "http://www.w3.org/2002/07/owl#hasValue": 8,
+              "http://www.w3.org/2000/01/rdf-schema#label": "Folder"
           }, {
-              "http://www.w3.org/2002/07/owl#hasValue": {
-                  "@value": "8",
-                  "@type": "http://www.w3.org/2001/XMLSchema#string"
-              },
-              "http://www.w3.org/2000/01/rdf-schema#label": {
-                  "@value": "Value set",
-                  "@type": "http://www.w3.org/2001/XMLSchema#string"
-              }
+              "http://www.w3.org/2002/07/owl#hasValue": 8,
+              "http://www.w3.org/2000/01/rdf-schema#label": "Value set"
           }, {
-              "http://www.w3.org/2002/07/owl#hasValue": {
-                  "@value": "2",
-                  "@type": "http://www.w3.org/2001/XMLSchema#string"
-              },
-              "http://www.w3.org/2000/01/rdf-schema#label": {
-                  "@value": "Reflexive property",
-                  "@type": "http://www.w3.org/2001/XMLSchema#string"
-              }
+              "http://www.w3.org/2002/07/owl#hasValue": 2,
+              "http://www.w3.org/2000/01/rdf-schema#label": "Reflexive property"
           }, {
-              "http://www.w3.org/2002/07/owl#hasValue": {
-                  "@value": "1",
-                  "@type": "http://www.w3.org/2001/XMLSchema#string"
-              },
-              "http://www.w3.org/2000/01/rdf-schema#label": {
-                  "@value": "Query template",
-                  "@type": "http://www.w3.org/2001/XMLSchema#string"
-              }
-          }
-          ],
-          "http://www.w3.org/2000/01/rdf-schema#comment": {
-              "@value": "A brief overview of the types of data stored in the Ontology",
-              "@type": "http://www.w3.org/2001/XMLSchema#string"
-          },
+              "http://www.w3.org/2002/07/owl#hasValue": 1,
+              "http://www.w3.org/2000/01/rdf-schema#label": "Query template"
+          }],
+          "http://www.w3.org/2000/01/rdf-schema#comment": "A brief overview of the types of data stored in the Ontology",
           "http://www.w3.org/1999/02/22-rdf-syntax-ns#type": {
               "@id": "http://endhealth.info/im#statsReport",
               "name": "Statistics Report"
           },
-          "http://www.w3.org/2000/01/rdf-schema#label": {
-              "@value": "Ontology concept types",
-              "@type": "http://www.w3.org/2001/XMLSchema#string"
-          }
+          "http://www.w3.org/2000/01/rdf-schema#label": "Ontology concept types"
       }
     });
     mockStore = {
