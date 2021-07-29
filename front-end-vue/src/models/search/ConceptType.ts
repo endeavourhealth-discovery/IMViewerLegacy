a38b8aa3465732da324122ba1b297c21c7bc705f
Kostas Kardatos <kostas.kardatos@answerdigital.com>
2021-07-29 16:20:37 +0100

    drop datatype from type filter list

--- front-end-vue/src/models/search/ConceptType.ts
+++ front-end-vue/src/models/search/ConceptType.ts
@@ -2,7 +2,6 @@ export class ConceptType {
   static Class = "http://www.w3.org/2002/07/owl#Class";
   static ObjectProperty = "http://www.w3.org/2002/07/owl#ObjectProperty";
   static DataProperty = "http://www.w3.org/2002/07/owl#DataProperty";
-  static DataType = "http://www.w3.org/ns/shacl#datatype";
   static Annotation = "http://www.w3.org/2002/07/owl#AnnotationProperty";
   static Individual = "http://www.w3.org/2002/07/owl#NamedIndividual";
   static Record = "http://endhealth.info/im#RecordType";
