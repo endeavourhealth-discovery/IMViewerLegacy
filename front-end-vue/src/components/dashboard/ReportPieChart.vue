e20bc766a4ac2444de539623674d99519a8acfef
matt-ergosoft <matt.hutcheson@ergosoft.co.uk>
2021-08-13 14:37:55 +0100

    added typing back to chartOptions

--- front-end-vue/src/components/dashboard/ReportPieChart.vue
+++ front-end-vue/src/components/dashboard/ReportPieChart.vue
@@ -58,7 +58,7 @@ export default defineComponent({
             }
           }
         }
-      } as any,
+      } as ChartOptions,
       realData: {} as number[],
       chartConceptTypes: new PieChartData(
         [
