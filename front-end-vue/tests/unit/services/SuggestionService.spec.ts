4984d438a1401b6ceecdf63b1d179f93101a8fac
matt-ergosoft <matt.hutcheson@ergosoft.co.uk>
2021-09-23 14:06:17 +0100

    added tests for config and sugestion service

--- front-end-vue/tests/unit/services/SuggestionService.spec.ts
+++ front-end-vue/tests/unit/services/SuggestionService.spec.ts
@@ -9,6 +9,8 @@ describe("SuggestionService.ts", () => {
   it("can get iri suggestions", async() => {
     const result = await SuggestionService.getIriSuggestions("testKeyword", "testWord");
     expect(axios.get).toHaveBeenCalledTimes(1);
+    const api = process.env.VUE_APP_API;
+    expect(axios.get).toHaveBeenCalledWith(api + "api/entity/referenceSuggestions", { params: { keyword: "testKeyword", word: "testWord" } });
     expect(result).toStrictEqual([{ iri: "testIri", name: "testName" }]);
   });
 });
