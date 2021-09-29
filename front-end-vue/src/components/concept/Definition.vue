200b3ed7d5d8691cf9a3ae1ef9fbdaee104a0a96
matt-ergosoft <matt.hutcheson@ergosoft.co.uk>
2021-09-29 14:18:49 +0100

    linter fixes

--- front-end-vue/src/components/concept/Definition.vue
+++ front-end-vue/src/components/concept/Definition.vue
@@ -2,7 +2,14 @@
   <div class="content-container">
     <div class="summary-container">
       <template v-for="(config, index) in configs" :key="index">
-        <component :is="config.type" :label="config.label" :data="concept[config.predicate]" :size="config.size" :id="config.type + index" :show="showItem(config, index)"> </component>
+        <component
+          :is="config.type"
+          :label="config.label"
+          :data="concept[config.predicate]"
+          :size="config.size"
+          :id="config.type + index"
+          :show="showItem(config, index)"
+        />
       </template>
     </div>
   </div>
@@ -44,25 +51,25 @@ export default defineComponent({
       if (config.type === "SectionDivider") {
         let i = index - 1;
         while (i > 0) {
-          const data = this.concept[this.configs[i].predicate]
+          const data = this.concept[this.configs[i].predicate];
           if (this.configs[i].type === "SectionDivider") {
             break;
           }
           hasDataResults.push(this.hasData(data));
-          i --;
+          i--;
         }
       } else if (config.type === "TextSectionHeader") {
         let i = index + 1;
-        const data = this.concept[this.configs[i].predicate]
+        const data = this.concept[this.configs[i].predicate];
         while (i < this.configs.length) {
           if (this.configs[i].type === "SectionDivider") {
             break;
           }
           hasDataResults.push(this.hasData(data));
-          i ++;
+          i++;
         }
       } else {
-        const data = this.concept[this.configs[index].predicate]
+        const data = this.concept[this.configs[index].predicate];
         hasDataResults.push(this.hasData(data));
       }
       const show = !hasDataResults.every(value => value === false);
@@ -75,16 +82,16 @@ export default defineComponent({
         result = false;
       }
       if (Array.isArray(data)) {
-        data.length ? result = true : result = false;
+        data.length ? (result = true) : (result = false);
       }
-      if (typeof(data) === "string") {
-        data ? result = true : result = false;
+      if (typeof data === "string") {
+        data ? (result = true) : (result = false);
       }
       if (Object.prototype.toString.call(data) === "[object Object]") {
         if (Object.prototype.hasOwnProperty.call(data, "count")) {
-          data.count ? result = true : result = false;
+          data.count ? (result = true) : (result = false);
         } else {
-          Object.keys(data).length ? result = true : result = false;
+          Object.keys(data).length ? (result = true) : (result = false);
         }
       }
       return result;
