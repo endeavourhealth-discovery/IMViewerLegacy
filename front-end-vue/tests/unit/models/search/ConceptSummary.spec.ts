4cfd9e2fcd24db60d6bd745bc66dcb1eac04345f
matt-ergosoft <matt.hutcheson@ergosoft.co.uk>
2021-10-06 17:28:35 +0100

    fixed conceptSummary TTIri refs from iri to @id for tests

--- front-end-vue/tests/unit/models/search/ConceptSummary.spec.ts
+++ front-end-vue/tests/unit/models/search/ConceptSummary.spec.ts
@@ -2,16 +2,26 @@ import { ConceptSummary } from "@/models/search/ConceptSummary";
 
 describe("ConceptSummary.ts", () => {
   it("has the correct fields", () => {
-    let summary = new ConceptSummary;
-    summary = { name: "TestName", iri: "TestIri", scheme: { name: "schemeName", iri: "schemeIri" }, code: "testCode", entityType: { elements: [{ name: "typeName", iri: "typeIri" }] }, isDescendentOf: [], weighting: 8, match: "testMatch", status: { name: "statusName", iri: "statusIri" } };
+    let summary = new ConceptSummary();
+    summary = {
+      name: "TestName",
+      iri: "TestIri",
+      scheme: { name: "schemeName", "@id": "schemeIri" },
+      code: "testCode",
+      entityType: { elements: [{ name: "typeName", "@id": "typeIri" }] },
+      isDescendentOf: [],
+      weighting: 8,
+      match: "testMatch",
+      status: { name: "statusName", "@id": "statusIri" }
+    };
     expect(summary.name).toBe("TestName");
     expect(summary.iri).toBe("TestIri");
-    expect(summary.scheme).toStrictEqual({ name: "schemeName", iri: "schemeIri" });
+    expect(summary.scheme).toStrictEqual({ name: "schemeName", "@id": "schemeIri" });
     expect(summary.code).toBe("testCode");
-    expect(summary.entityType).toStrictEqual({ elements: [{ name: "typeName", iri: "typeIri" }] });
+    expect(summary.entityType).toStrictEqual({ elements: [{ name: "typeName", "@id": "typeIri" }] });
     expect(summary.isDescendentOf).toStrictEqual([]);
     expect(summary.weighting).toBe(8);
     expect(summary.match).toBe("testMatch");
-    expect(summary.status).toStrictEqual({ name: "statusName", iri: "statusIri" });
-  })
+    expect(summary.status).toStrictEqual({ name: "statusName", "@id": "statusIri" });
+  });
 });
