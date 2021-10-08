f25a384c374f8d00ebc215b613c3faad67416dbf
matt-voror <matt@voror.co.uk>
2021-10-08 09:58:03 +0100

    swapped isObject to have optional keys input

--- front-end-vue/src/helpers/DataTypeCheckers.ts
+++ front-end-vue/src/helpers/DataTypeCheckers.ts
@@ -6,12 +6,18 @@ export function isArrayHasLength(array: any[]): boolean {
   }
 }
 
-export function isObjectHasKeys(object: any, keys: string[]) {
+export function isObjectHasKeys(object: any, keys?: string[]) {
   let result = true;
-  if (!(Object.prototype.toString.call(object) === "[object Object]")) result = false;
-  const objectKeys = Object.keys(object);
-  keys.forEach(key => {
-    if (!objectKeys.includes(key)) result = false;
-  });
+  if (!keys) {
+    if (!(Object.prototype.toString.call(object) === "[object Object]")) result = false;
+    if (!Object.keys(object).length) result = false;
+  } else {
+    if (!(Object.prototype.toString.call(object) === "[object Object]")) result = false;
+    const objectKeys = Object.keys(object);
+    keys.forEach(key => {
+      if (!objectKeys.includes(key)) result = false;
+    });
+  }
+
   return result;
 }
