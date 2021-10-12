2a0e6bbb144e5383be3dd5390392ec7a54b69de2
matt-voror <matt@voror.co.uk>
2021-10-12 09:20:14 +0100

    extracted mounted service call to own methodupdated reportTable tests for new service return object

--- front-end-vue/tests/unit/components/dashboard/ReportTable.spec.ts
+++ front-end-vue/tests/unit/components/dashboard/ReportTable.spec.ts
@@ -1,65 +1,40 @@
-import { shallowMount } from "@vue/test-utils";
+import { flushPromises, shallowMount } from "@vue/test-utils";
 import ReportTable from "@/components/dashboard/ReportTable.vue";
 import Card from "primevue/card";
 import DataTable from "primevue/datatable";
 import ProgressSpinner from "primevue/progressspinner";
 import Column from "primevue/column";
 import EntityService from "@/services/EntityService";
+import { RDFS } from "@/vocabulary/RDFS";
+import { IM } from "@/vocabulary/IM";
 
 describe("ReportTable.vue", () => {
   let wrapper: any;
   let mockStore: any;
   let mockToast: any;
-
-  beforeEach(() => {
-    jest.clearAllMocks();
-    EntityService.getPartialEntity = jest.fn().mockResolvedValue({
-      "@id": "http://endhealth.info/im#ontologyOverview",
-      "http://endhealth.info/im#hasStatsReportEntry": [
-        {
-          "http://www.w3.org/2002/07/owl#hasValue": {
-            "@value": "8",
-            "@type": "http://www.w3.org/2001/XMLSchema#string"
-          },
-          "http://www.w3.org/2000/01/rdf-schema#label": {
-            "@value": "Value sets",
-            "@type": "http://www.w3.org/2001/XMLSchema#string"
-          }
-        },
-        {
-          "http://www.w3.org/2002/07/owl#hasValue": {
-            "@value": "1973",
-            "@type": "http://www.w3.org/2001/XMLSchema#string"
-          },
-          "http://www.w3.org/2000/01/rdf-schema#label": {
-            "@value": "Data models",
-            "@type": "http://www.w3.org/2001/XMLSchema#string"
-          }
-        },
-        {
-          "http://www.w3.org/2002/07/owl#hasValue": {
-            "@value": "1124984",
-            "@type": "http://www.w3.org/2001/XMLSchema#string"
-          },
-          "http://www.w3.org/2000/01/rdf-schema#label": {
-            "@value": "Ontology",
-            "@type": "http://www.w3.org/2001/XMLSchema#string"
-          }
-        }
-      ],
-      "http://www.w3.org/2000/01/rdf-schema#comment": {
-        "@value": "A brief overview of the concepts stored in the Ontology",
-        "@type": "http://www.w3.org/2001/XMLSchema#string"
+  let reportData = {
+    "@id": "http://endhealth.info/im#ontologyOverview",
+    "http://endhealth.info/im#hasStatsReportEntry": [
+      {
+        "http://www.w3.org/2002/07/owl#hasValue": 8,
+        "http://www.w3.org/2000/01/rdf-schema#label": "Value sets"
       },
-      "http://www.w3.org/1999/02/22-rdf-syntax-ns#type": {
-        "@id": "http://endhealth.info/im#statsReport",
-        name: "Statistics Report"
+      {
+        "http://www.w3.org/2002/07/owl#hasValue": 1973,
+        "http://www.w3.org/2000/01/rdf-schema#label": "Data models"
       },
-      "http://www.w3.org/2000/01/rdf-schema#label": {
-        "@value": "Ontology overview",
-        "@type": "http://www.w3.org/2001/XMLSchema#string"
+      {
+        "http://www.w3.org/2002/07/owl#hasValue": 1124984,
+        "http://www.w3.org/2000/01/rdf-schema#label": "Ontology"
       }
-    });
+    ],
+    "http://www.w3.org/2000/01/rdf-schema#comment": "A brief overview of the concepts stored in the Ontology",
+    "http://www.w3.org/2000/01/rdf-schema#label": "Ontology overview"
+  };
+
+  beforeEach(async () => {
+    jest.clearAllMocks();
+    EntityService.getPartialEntity = jest.fn().mockResolvedValue(reportData);
     mockStore = {
       commit: jest.fn()
     };
@@ -73,15 +48,61 @@ describe("ReportTable.vue", () => {
         mocks: { $store: mockStore, $toast: mockToast }
       }
     });
+
+    await flushPromises();
+    jest.clearAllMocks();
   });
 
-  it("sets loading for mounted", () => {
-    expect(mockStore.commit).toHaveBeenCalledTimes(2);
-    expect(mockStore.commit).toHaveBeenNthCalledWith(1, "updateLoading", { key: "reportTable_im:Test", value: true });
-    expect(mockStore.commit).toHaveBeenLastCalledWith("updateLoading", { key: "reportTable_im:Test", value: false });
+  it("mounts", () => {
+    expect(wrapper.vm.tableData).toStrictEqual([
+      { count: 8, label: "Value sets" },
+      { count: 1973, label: "Data models" },
+      { count: 1124984, label: "Ontology" }
+    ]);
   });
 
-  it("calls IndividualService.getIndividual", () => {
+  it("can getReportTableData ___ correct object", async () => {
+    wrapper.vm.tableData = [] as { count: number; label: string }[];
+    wrapper.vm.getReportTableData();
+    expect(mockStore.commit).toHaveBeenCalledWith("updateLoading", {
+      key: "reportTable_" + "im:Test",
+      value: true
+    });
     expect(EntityService.getPartialEntity).toHaveBeenCalledTimes(1);
+    expect(EntityService.getPartialEntity).toHaveBeenCalledWith("im:Test", [RDFS.LABEL, RDFS.COMMENT, IM.STATS_REPORT_ENTRY]);
+    await flushPromises();
+    expect(wrapper.vm.tableData).toStrictEqual([
+      { count: 8, label: "Value sets" },
+      { count: 1973, label: "Data models" },
+      { count: 1124984, label: "Ontology" }
+    ]);
+    expect(mockStore.commit).toHaveBeenCalledTimes(2);
+    expect(mockStore.commit).toHaveBeenLastCalledWith("updateLoading", {
+      key: "reportTable_" + "im:Test",
+      value: false
+    });
+  });
+
+  it("can getReportTableData ___ object missing key", async () => {
+    EntityService.getPartialEntity = jest.fn().mockResolvedValue({
+      "@id": "http://endhealth.info/im#ontologyOverview",
+      "http://www.w3.org/2000/01/rdf-schema#comment": "A brief overview of the concepts stored in the Ontology",
+      "http://www.w3.org/2000/01/rdf-schema#label": "Ontology overview"
+    });
+    wrapper.vm.tableData = [] as { count: number; label: string }[];
+    wrapper.vm.getReportTableData();
+    expect(mockStore.commit).toHaveBeenCalledWith("updateLoading", {
+      key: "reportTable_" + "im:Test",
+      value: true
+    });
+    expect(EntityService.getPartialEntity).toHaveBeenCalledTimes(1);
+    expect(EntityService.getPartialEntity).toHaveBeenCalledWith("im:Test", [RDFS.LABEL, RDFS.COMMENT, IM.STATS_REPORT_ENTRY]);
+    await flushPromises();
+    expect(wrapper.vm.tableData).toStrictEqual([]);
+    expect(mockStore.commit).toHaveBeenCalledTimes(2);
+    expect(mockStore.commit).toHaveBeenLastCalledWith("updateLoading", {
+      key: "reportTable_" + "im:Test",
+      value: false
+    });
   });
 });
