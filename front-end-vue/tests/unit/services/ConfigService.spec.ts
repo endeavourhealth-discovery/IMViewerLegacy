fa25f9a7a1906b74d5649905869d9908491da48a
matt-voror <matt@voror.co.uk>
2021-11-22 11:35:29 +0000

    added getXmlSchemaDataTypes tests

--- front-end-vue/tests/unit/services/ConfigService.spec.ts
+++ front-end-vue/tests/unit/services/ConfigService.spec.ts
@@ -35,6 +35,13 @@ describe("ConfigService.ts ___ axios success", () => {
     expect(axios.get).toHaveBeenCalledWith(api + "api/config/defaultPredicateNames");
     expect(result).toStrictEqual(["test config"]);
   });
+
+  it("can getXmlSchemaDataTypes", async () => {
+    const result = await ConfigService.getXmlSchemaDataTypes();
+    expect(axios.get).toHaveBeenCalledTimes(1);
+    expect(axios.get).toHaveBeenCalledWith(api + "api/config/xmlSchemaDataTypes");
+    expect(result).toStrictEqual(["test config"]);
+  });
 });
 
 describe("ConfigService.ts ___ axios fail", () => {
@@ -70,5 +77,12 @@ describe("ConfigService.ts ___ axios fail", () => {
     expect(axios.get).toHaveBeenCalledTimes(1);
     expect(axios.get).toHaveBeenCalledWith(api + "api/config/defaultPredicateNames");
     expect(result).toStrictEqual({});
+  });
+
+  it("can get xmlSchemaDataTypes", async () => {
+    const result = await ConfigService.getXmlSchemaDataTypes();
+    expect(axios.get).toHaveBeenCalledTimes(1);
+    expect(axios.get).toHaveBeenCalledWith(api + "api/config/xmlSchemaDataTypes");
+    expect(result).toStrictEqual([]);
   });
 });
