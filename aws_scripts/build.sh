6ce7fff23171228f2d24a8431ec9ca0f69a9c9ce
Richard Collier <gitkraken@tirinoarim.co.uk>
2021-12-16 14:43:04 +0000

    Gradle build

--- aws_scripts/build.sh
+++ aws_scripts/build.sh
@@ -2,29 +2,22 @@
 
 mkdir badges
 
-# Artifact
-artifact=$( xmllint --xpath "/*[local-name() = 'project']/*[local-name() = 'artifactId']/text()" pom.xml )
-
-# Version
-version=$( xmllint --xpath "/*[local-name() = 'project']/*[local-name() = 'version']/text()" pom.xml )
-version=${version/-/--} # Hyphen escaping required by shields.io
-
 # Update badges pre-build
 echo "https://img.shields.io/badge/Build-In_progress-orange.svg"
 curl -s "https://img.shields.io/badge/Build-In_progress-orange.svg" > badges/build.svg
 
-echo "https://img.shields.io/badge/Version-$version-orange.svg"
-curl -s "https://img.shields.io/badge/Version-$version-orange.svg" > badges/version.svg
+echo "https://img.shields.io/badge/Version-1.0.0-orange.svg"
+curl -s "https://img.shields.io/badge/Version-1.0.0-orange.svg" > badges/version.svg
 
 echo "https://img.shields.io/badge/Unit_Tests-Pending-orange.svg"
 curl -s "https://img.shields.io/badge/Unit_Tests-Pending-orange.svg" > badges/unit-test.svg
 
 # Sync with S3
-aws s3 cp badges s3://endeavour-codebuild-output/badges/${artifact}/ --recursive --acl public-read
+aws s3 cp badges s3://endeavour-codebuild-output/badges/IMViewer/ --recursive --acl public-read
 
 # Build
 { #try
-    eval $* &&
+    ./gradlew assemble
     buildresult=0
 } || { #catch
     buildresult=1
@@ -45,9 +38,14 @@ echo "https://img.shields.io/badge/Version-$version-$badge_colour.svg"
 curl -s "https://img.shields.io/badge/Version-$version-$badge_colour.svg" > badges/version.svg
 
 # Unit tests
-failures=$( xmllint --xpath 'string(//testsuite/@failures) + string(//testsuite/@errors)' api/target/surefire-reports/TEST-*.xml )
+{ #try
+    ./gradlew check
+    testresult=0
+} || { #catch
+    testresult=1
+}
 
-if [[ "$failures" -gt "0" ]] ; then
+if [[ "$testresult" -gt "0" ]] ; then
         badge_status=failing
         badge_colour=red
 else
@@ -59,6 +57,6 @@ echo "Generating badge 'https://img.shields.io/badge/Unit_Tests-$badge_status-$b
 curl -s "https://img.shields.io/badge/Unit_Tests-$badge_status-$badge_colour.svg" > badges/unit-test.svg
 
 # Sync with S3
-aws s3 cp badges s3://endeavour-codebuild-output/badges/${artifact}/ --recursive --acl public-read
+aws s3 cp badges s3://endeavour-codebuild-output/badges/IMViewer/ --recursive --acl public-read
 
 exit ${buildresult}
