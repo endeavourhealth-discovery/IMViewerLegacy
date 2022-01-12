179da449aad20865bddc2e50b73dce41978218d5
Richard Collier <gitkraken@tirinoarim.co.uk>
2022-01-12 19:03:19 +0000

    build script fix

--- aws_scripts/build.sh
+++ aws_scripts/build.sh
@@ -10,6 +10,8 @@ artifact='IMViewer'
 # Version
 version='1.0.0'
 
+returncode = 0
+
 # Update badges pre-build
 echo "https://img.shields.io/badge/Build-In_progress-orange.svg"
 curl -s "https://img.shields.io/badge/Build-In_progress-orange.svg" > badges/build.svg
@@ -35,6 +37,7 @@ aws s3 cp badges s3://endeavour-codebuild-output/badges/${artifact}/ --recursive
 if [[ "$buildresult" -gt "0" ]] ; then
         badge_status=failing
         badge_colour=red
+        returncode = 1
 else
         badge_status=passing
         badge_colour=green
@@ -56,6 +59,7 @@ curl -s "https://img.shields.io/badge/Version-$version-$badge_colour.svg" > badg
 if [[ "$testresult" -gt "0" ]] ; then
         badge_status=failing
         badge_colour=red
+        returncode = 1
 else
         badge_status=passing
         badge_colour=green
@@ -67,4 +71,4 @@ curl -s "https://img.shields.io/badge/Unit_Tests-$badge_status-$badge_colour.svg
 # Sync with S3
 aws s3 cp badges s3://endeavour-codebuild-output/badges/${artifact}/ --recursive --acl public-read --region eu-west-2
 
-exit ${buildresult}
+exit ${returncode}
